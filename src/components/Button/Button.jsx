42256478997e9f5ad47453c803d86c577acb4ac5
ryamagushi <ryamagushi@uolinc.com>
2023-10-29 23:09:23 -0300

    feat: style login

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,12 +4,22 @@ const stylesVariant = {
   primary: styles.primary,
   secondary: styles.secondary,
 };
-export const Button = ({ title, variant = "primary", path, onClick }) => {
+export const Button = ({
+  title,
+  variant = "primary",
+  path,
+  onClick,
+  onClickFailure,
+}) => {
   const buttonId = `${Math.floor(Math.random() * 1000)}`;
   const onClickButton = path
     ? () => {
-        window.location.href = path;
-        if (onClick) onClick();
+        try {
+          if (onClick) onClick();
+          window.location.href = path;
+        } catch {
+          if (onClickFailure) onClickFailure();
+        }
       }
     : onClick;
 
