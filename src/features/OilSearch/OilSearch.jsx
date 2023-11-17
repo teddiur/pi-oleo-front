2d2a412c63658f3faf8a9568732021bf46df3a93
ryamagushi <ryamagushi@uolinc.com>
2023-11-17 19:02:23 -0300

    add seleção de oleo para coleta

--- src/features/OilSearch/OilSearch.jsx
+++ src/features/OilSearch/OilSearch.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../../components/Button/Button";
 import { getAxios } from "../../api";
 import Spinner from "../../components/Spinner";
@@ -27,42 +27,99 @@ function proximoDiaSemana(numeroDiaSemana) {
 const OilSearch = () => {
   const [chosenDistrict, setChosenDistrict] = useState([]);
   const [oils, setOils] = useState([]);
+  const [selectedOil, setSelectedOil] = useState({});
   const [searched, setSearched] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const dialogRef = useRef();
 
   const totalVolume = oils.reduce((acc, cur) => acc + cur.oil_quantity, 0);
+  const getAvailableOil = async () => {
+    setIsLoading(true);
+    const a = document.getElementById("district");
+    const district = a.value;
+    if (!district) alert("escolhe o bairro");
+    try {
+      const { data } = await getAxios().get("/oil/open/" + district);
+      setChosenDistrict(district);
+      const oilsAvailable = data.sort((a, b) => {
+        if (a.day > b.day) return 1;
+        if (a.day < b.day) return -1;
+        return 0;
+      });
+
+      setOils(oilsAvailable);
+    } catch {
+      console.log("hm");
+    } finally {
+      setSearched(true);
+      setIsLoading(false);
+    }
+  };
+
+  const onClick = async () => {
+    setIsLoading(true);
+    await getAxios().post("/oil/collect", {
+      day: selectedOil.day.toISOString().slice(0, 10),
+      ids: [selectedOil.id],
+    });
+    dialogRef.current.close();
+    await getAvailableOil();
+    setIsLoading(false);
+  };
   return (
     <>
       <Button
         title="Buscar"
         id="buscar"
         buttonStyle={{ marginBottom: "3rem", marginTop: "1rem" }}
-        onClick={async () => {
-          setIsLoading(true);
-          const a = document.getElementById("district");
-          const district = a.value;
-          if (!district) alert("escolhe o bairro");
-          try {
-            const { data } = await getAxios().get("/oil/open/" + district);
-            setChosenDistrict(district);
-            const oilsAvailable = data.sort((a, b) => {
-              if (a.day > b.day) return 1;
-              if (a.day < b.day) return -1;
-              return 0;
-            });
-
-            setOils(oilsAvailable);
-          } catch {
-            console.log("hm");
-          } finally {
-            setSearched(true);
-            setIsLoading(false);
-          }
-        }}
+        onClick={getAvailableOil}
       />
       {isLoading && <Spinner />}
       {searched && (
         <>
+          <dialog
+            ref={dialogRef}
+            className={styles.dialog}
+            role="dialog"
+            aria-labelledby="Confirmação de retirada"
+          >
+            <p>
+              Você deseja retirar {selectedOil.oil_quantity} litro
+              {selectedOil.oil_quantity > 1 && "s"} de óleo em{" "}
+              {selectedOil.address} no dia {selectedOil.displayDay} (próxim
+              {"domingo" == selectedOil.day_available ? "o " : "a "}
+              {selectedOil.day_available})?
+            </p>
+            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+              <Button
+                title="Não"
+                id="Não"
+                buttonStyle={{
+                  marginBottom: "1rem",
+                  marginTop: "1rem",
+                  minWidth: "fit-content",
+                  backgroundColor: "red",
+                  padding: "0.75rem 2rem",
+                }}
+                onClick={() => dialogRef.current.close()}
+              >
+                Não
+              </Button>
+              <Button
+                title="Sim"
+                id="Sim"
+                buttonStyle={{
+                  marginBottom: "1rem",
+                  marginTop: "1rem",
+                  padding: "0.75 2rem",
+                  minWidth: "fit-content",
+                }}
+                onClick={onClick}
+              >
+                Não
+              </Button>
+            </div>
+          </dialog>
           <h1
             style={{
               color: "#515251",
@@ -87,26 +144,36 @@ const OilSearch = () => {
             Lista de endereços para retirada no Bairro {chosenDistrict}
           </h1>
           {oils.map(({ address, oil_quantity, day_available, id }) => {
-            const onClick = () => {
-              const translator = {
-                domingo: 0,
-                segunda: 1,
-                terca: 2,
-                quarta: 3,
-                quinta: 4,
-                sexta: 5,
-                sabado: 6,
-              };
-
-              const collectDay = proximoDiaSemana(translator[day_available]);
-
-              getAxios().post("/oil/collect", {
-                day: collectDay.toISOString("pt-BR").slice(0, 10),
-                ids: [id],
-              });
-            };
             return (
-              <div key={address} className={styles.select} onClick={onClick}>
+              <div
+                key={address}
+                className={styles.select}
+                onClick={() => {
+                  const translator = {
+                    domingo: 0,
+                    segunda: 1,
+                    terca: 2,
+                    quarta: 3,
+                    quinta: 4,
+                    sexta: 5,
+                    sabado: 6,
+                  };
+
+                  const day = proximoDiaSemana(translator[day_available]);
+
+                  setSelectedOil({
+                    address,
+                    oil_quantity,
+                    day_available,
+                    id,
+                    day,
+                    displayDay: day.toLocaleDateString("pt-BR"),
+                  });
+                  dialogRef.current.showModal();
+                }}
+                role="button"
+                aria-pressed="false"
+              >
                 <p
                   style={{
                     margin: 0,
