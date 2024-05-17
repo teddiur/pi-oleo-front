07813b9da766085233bbeec20dab322c484fe821
Rodrigo Queiroz <ryamagushi@uolinc.com>
2024-05-16 23:34:44 -0300

    menu

--- src/components/HamburguerMenu/HamburguerMenu.jsx
+++ src/components/HamburguerMenu/HamburguerMenu.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import hamburguer from "../../images/hamburguer.svg";
+import style from "./HamburguerMenu.module.css";
 
 export const HamburguerMenu = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <>
-      {isOpen ? (
-        <p>WIP</p>
-      ) : (
-        <img
-          src={hamburguer}
-          alt="Menu"
-          width={20}
-          height={20}
-          onClick={() => {
-            setIsOpen(true);
-          }}
-        />
-      )}
+      <div className={style.large_menu}>
+        <a href="login">Login</a>
+        <a href="contato">Contato</a>
+        <a href="perguntas-frequentes">Dúvidas</a>
+        <a href="onde-descartar">Onde descartar?</a>
+        <a href="cadastrar">Criar conta</a>
+      </div>
+      <div className={style.small_menu}>
+        {isOpen ? (
+          <div className={style.container}>
+            <a href="login">Login</a>
+            <a href="contato">Contato</a>
+            <a href="perguntas-frequentes">Dúvidas</a>
+            <a href="onde-descartar">Onde descartar?</a>
+            <a href="cadastrar">Criar conta</a>
+          </div>
+        ) : (
+          <img
+            {...hamburguer}
+            alt="Menu"
+            width={20}
+            height={20}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          />
+        )}
+      </div>
     </>
   );
 };
